0ed9a305848f3ba842c180af2707dbaed8b92a77
Ashish <ashishdas.1997@gmail.com>
2020-05-28 20:37:50 +0530

    Fetching data to Firestore Database and displaying on Screen

--- lib/screens/articles.dart
+++ lib/screens/articles.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:savaynewdemo/screens/dashboard.dart';
 
@@ -7,21 +9,100 @@ class Articles extends StatefulWidget {
 }
 
 class _ArticlesState extends State<Articles> {
+  Firestore firestore = Firestore.instance;
+  List<DocumentSnapshot> loadedArticles = [];
+  bool expanded = false;
+  bool loading = true;
+  getArticles() async {
+    setState(() {
+      loading = true;
+    });
+    QuerySnapshot q = await firestore.collection('articles').getDocuments();
+    loadedArticles = q.documents;
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getArticles();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Articles'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => AddArticle()));
-            },
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Articles'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => AddArticle()));
+              },
+            ),
+          ],
+        ),
+        body: loading == true
+            ? Container(
+                child: Center(
+                  child: Text('Loading'),
+                ),
+              )
+            : Container(
+                child: loadedArticles.length == 0
+                    ? Center(
+                        child: Text(' No Articles to display'),
+                      )
+                    : ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: loadedArticles.length,
+                        itemBuilder: (BuildContext ctx, int index) {
+                          return Column(
+                            children: <Widget>[
+                              ListTile(
+                                title:
+                                    Text(loadedArticles[index].data['title']),
+                                leading: CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      loadedArticles[index].data['imageLink']),
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(expanded
+                                      ? Icons.expand_less
+                                      : Icons.expand_more),
+                                  onPressed: () {
+                                    setState(() {
+                                      expanded = true;
+                                    });
+                                  },
+                                ),
+                              ),
+                              if (expanded)
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 4),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: <Widget>[
+                                      Text(
+                                          'Category: ${loadedArticles[index].data['category']}'),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(
+                                          'Content Type: ${loadedArticles[index].data['contentType']}')
+                                    ],
+                                  ),
+                                ),
+                            ],
+                          );
+                        },
+                      )));
   }
 }
